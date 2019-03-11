d1a552960561b352f5f889d58cab791be27479c5
calebfavor <calebfavor@gmail.com>
2019-03-11 14:08:22 -0700

    Adding extra find typehint for users repo

--- src/Repositories/UserRepository.php
+++ src/Repositories/UserRepository.php
@@ -11,6 +11,7 @@ use Railroad\Usora\Managers\UsoraEntityManager;
 /**
  * Class UserRepository
  *
+ * @method User find($id, $lockMode = null, $lockVersion = null)
  * @method User findOneBy(array $criteria, array $orderBy = null)
  * @method User[] findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  * @method User[] findAll()
