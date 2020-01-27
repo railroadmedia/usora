56763c203c857acd4d17f43a20c9cb3b7e44a1d6
calebfavor <calebfavor@gmail.com>
2020-01-27 14:35:39 -0800

    Making email column unique.

---
+++ migrations/2020_01_27_189342_make_email_unique_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MakeEmailUniqueUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection(config('usora.database_connection_name'))
+            ->table(
+                config('usora.tables.users'),
+                function (Blueprint $table) {
+
+                    $table->unique('email', 'unique_email');
+                }
+            );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection(config('usora.database_connection_name'))
+            ->table(
+                config('usora.tables.users'),
+                function (Blueprint $table) {
+
+                    $table->dropIndex('unique_email');
+
+                }
+            );
+    }
+}
