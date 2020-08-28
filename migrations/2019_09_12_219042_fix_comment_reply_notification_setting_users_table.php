8ef50c08fc4f503a05556f24d646efd23ba589b2
calebfavor <caleb@drumeo.com>
2020-08-28 10:45:50 -0700

    Fixing migration breaking due to bug in dbal/laravel. https://github.com/laravel/framework/issues/30539

--- migrations/2019_09_12_219042_fix_comment_reply_notification_setting_users_table.php
+++ migrations/2019_09_12_219042_fix_comment_reply_notification_setting_users_table.php
@@ -26,9 +26,12 @@ class FixCommentReplyNotificationSettingUsersTable extends Migration
                 config('usora.tables.users'),
                 function (Blueprint $table) {
 
-                    $table->boolean('notify_on_lesson_comment_reply')
-                        ->default(true)
-                        ->change();
+                    DB::statement("ALTER TABLE usora_users CHANGE notify_on_lesson_comment_reply notify_on_lesson_comment_reply TINYINT(1) DEFAULT '1' NOT NULL");
+
+                    // broken due to dbal/laravel bug
+//                    $table->boolean('notify_on_lesson_comment_reply')
+//                        ->default(true)
+//                        ->change();
                 }
             );
     }
@@ -45,9 +48,12 @@ class FixCommentReplyNotificationSettingUsersTable extends Migration
                 config('usora.tables.users'),
                 function (Blueprint $table) {
 
-                    $table->string('notify_on_lesson_comment_reply')
-                        ->default(true)
-                        ->change();
+                    DB::statement("ALTER TABLE usora_users CHANGE notify_on_lesson_comment_reply notify_on_lesson_comment_reply VARCHAR(190) DEFAULT '1' NOT NULL");
+
+                    // broken due to dbal/laravel bug
+//                    $table->string('notify_on_lesson_comment_reply')
+//                        ->default(true)
+//                        ->change();
 
                 }
             );
